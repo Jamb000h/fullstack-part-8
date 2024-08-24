2620a35f27d736b8b274c0b2c1bd3e1682534b65
Jonne Kanerva <8905786+Jamb000h@users.noreply.github.com>
2024-08-24 21:28:13 +0300

    8.4

--- library-backend.js
+++ library-backend.js
@@ -115,7 +115,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!,
     authorCount: Int!,
-    allBooks: [Book!]!,
+    allBooks(author: String): [Book!]!,
     allAuthors: [Author!]!
   }
 `;
@@ -124,7 +124,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => args.author ? books.filter(book => book.author === args.author) : books,
     allAuthors: () => {
       return authors.map((author) => {
         return {
