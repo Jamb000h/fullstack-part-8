cfb065bdb3993d984abe98acedecff35dbf97998
Jonne Kanerva <8905786+Jamb000h@users.noreply.github.com>
2024-08-24 22:33:02 +0300

    8.9

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
-import { ALL_AUTHORS } from "./queries";
+import { ALL_AUTHORS, ALL_BOOKS } from "./queries";
 import { useQuery } from '@apollo/client'
 
 const App = () => {
   const [page, setPage] = useState("authors");
   const authors = useQuery(ALL_AUTHORS)
+  const books = useQuery(ALL_BOOKS)
 
   return (
     <div>
@@ -19,7 +20,7 @@ const App = () => {
 
       <Authors show={page === "authors"} authors={authors?.data?.allAuthors ?? []} />
 
-      <Books show={page === "books"} />
+      <Books show={page === "books"} books={books?.data?.allBooks ?? []}/>
 
       <NewBook show={page === "add"} />
     </div>
