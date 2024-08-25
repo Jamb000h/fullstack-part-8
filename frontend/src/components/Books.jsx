281c028cdd71f3f24bb9366b3c9bf23ac734ea3d
Jonne Kanerva <8905786+Jamb000h@users.noreply.github.com>
2024-08-25 15:40:17 +0300

    8.19

--- frontend/src/components/Books.jsx
+++ frontend/src/components/Books.jsx
@@ -1,12 +1,21 @@
+import {useState} from "react"
+
 const Books = (props) => {
+  const [selectedGenre, setSelectedGenre] = useState("all genres")
   if (!props.show) {
     return null
   }
 
+  const genres = new Set(props.books.flatMap(book => book.genres))
+
+  const books = selectedGenre === "all genres" ? props.books : props.books.filter(book => book.genres.includes(selectedGenre))
+
   return (
     <div>
       <h2>books</h2>
-
+      {selectedGenre !== "all genres" && (
+        <p>in genre <strong>{selectedGenre}</strong></p>
+      )}
       <table>
         <tbody>
           <tr>
@@ -14,7 +23,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {props.books.map((a) => (
+          {books.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -23,6 +32,10 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+      {Array.from(genres).map(genre => {
+        return <button onClick={() => setSelectedGenre(genre)}>{genre}</button>
+      })}
+      <button onClick={() => setSelectedGenre("all genres")}>all genres</button>
     </div>
   )
 }
