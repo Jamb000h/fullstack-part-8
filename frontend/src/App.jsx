c3d834bc24ff40f52c6eb70b9591d914d916ce09
Jonne Kanerva <8905786+Jamb000h@users.noreply.github.com>
2024-08-25 15:30:59 +0300

    8.18

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,31 +2,55 @@ import { useState } from "react";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
+import LoginForm from "./components/LoginForm";
 import { ALL_AUTHORS, ALL_BOOKS } from "./queries";
-import { useQuery } from '@apollo/client'
+import { useQuery, useApolloClient } from "@apollo/client";
 import EditAuthor from "./components/EditAuthor";
 
 const App = () => {
   const [page, setPage] = useState("authors");
-  const authors = useQuery(ALL_AUTHORS)
-  const books = useQuery(ALL_BOOKS)
+  const [token, setToken] = useState(null);
+  const client = useApolloClient()
+  const authors = useQuery(ALL_AUTHORS);
+  const books = useQuery(ALL_BOOKS);
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
 
   return (
     <div>
       <div>
         <button onClick={() => setPage("authors")}>authors</button>
         <button onClick={() => setPage("books")}>books</button>
-        <button onClick={() => setPage("add")}>add book</button>
-        <button onClick={() => setPage("editAuthor")}>edit author</button>
+        {token && (
+          <>
+            <button onClick={() => setPage("addBook")}>add book</button>
+            <button onClick={() => setPage("editAuthor")}>edit author</button>
+            <button onClick={logout}>logout</button>
+          </>
+        )}
+        {!token && (
+          <button onClick={() => setPage("loginForm")}>login form</button>
+        )}
       </div>
 
-      <Authors show={page === "authors"} authors={authors?.data?.allAuthors ?? []} />
+      <Authors
+        show={page === "authors"}
+        authors={authors?.data?.allAuthors ?? []}
+      />
 
-      <Books show={page === "books"} books={books?.data?.allBooks ?? []}/>
+      <Books show={page === "books"} books={books?.data?.allBooks ?? []} />
 
-      <NewBook show={page === "add"} />
+      <NewBook show={page === "addBook"} />
 
-      <EditAuthor show={page === "editAuthor"} authors={authors?.data?.allAuthors ?? []} />
+      <EditAuthor
+        show={page === "editAuthor"}
+        authors={authors?.data?.allAuthors ?? []}
+      />
+
+      <LoginForm show={page === "loginForm"} setToken={setToken} setPage={setPage} />
     </div>
   );
 };
